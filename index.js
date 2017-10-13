47b058eeedef8195ebcd859b407b509129a60d04
Nicole Campbell <nscampbell303@gmail.com>
2017-10-13 16:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,29 +17,4 @@ function increaseRankBy(n) {
 function deepestChild() {
   let gnode = document.querySelector('div#grand-node div div div div')
   return gnode
-  // var newArr = []
-  //
-  // while(gnode) {
-  //   if (gnode.children) {
-  //   //  gnode = children.length
-  //   } else {
-  //     newArr
-  //   }
-  // }
-  //
-  // function find(node) {
-  //   let current = gnode
-  //   let next = []
-  //
-  //   while(current) {
-  //     if (Array.isArray(current)) {
-  //       for (let i = 0; i < current.length; i++) {
-  //         next.push(current[i])
-  //       }
-  //     }
-  //     current = next.shift()
-  //   }
-  //   return null
-  // }
-  // s => (typeof s === 'string' && s < 5)
 }
